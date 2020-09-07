d9592966dd9fe8e895b54da92ee463b367d3a53d
ozair <truemail785@gmail.com>
2020-09-07 22:25:42 +0500

    Updates

---
+++ precache-manifest.e5c4374083467f81fb4dc7e442259e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90716dec1664f30b599dbba28d2d6f9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f7a8f8afeec336310b09",
+    "url": "/calculator/static/css/main.8c87b2c4.chunk.css"
+  },
+  {
+    "revision": "d5b1d29fe0bd750cc601",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a8f8afeec336310b09",
+    "url": "/calculator/static/js/main.2eb5bcb5.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
